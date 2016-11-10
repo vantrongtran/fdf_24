5c54f9b11c9efc6daedd281d504eb9cb702fd2b0
vantrongtran <vntrngtrn@yahoo.com.vn>
2016-11-10 14:40:58 +0700

    sign up with divise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,14 @@ class User < ApplicationRecord
   has_many :orders
   has_many :comments
   has_many :ratings
+
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
   enum role: [:user, :admin]
+
+  mount_uploader :avatar, PictureUploader
+
+  validates :name, presence: true, length: {maximum: 50}
+  validates :chatwork_id, presence:true
+
 end
